b30b110c6f0c59e5716b3c357a13594e3d2d599b
Przemysław Mirowski <miretpl@gmail.com>
2020-11-21 22:13:27 +0100

    update board view

--- src/app/board/view-board/view-board.component.ts
+++ src/app/board/view-board/view-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,11 +8,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./view-board.component.css']
 })
 export class ViewBoardComponent implements OnInit {
-  board_id: number;
+  boardId: number;
+  boardName: string;
+  boardGroup: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.board_id = this.activatedRoute.snapshot.params.board_id;
+    this.boardId = this.activatedRoute.snapshot.params.board_id;
+    
+    this.boardName = "test board 1";
+
+    this.boardGroup = new FormGroup({
+      name: new FormControl(this.boardName, Validators.required)
+      });
+  }
+
+  updateBoardName() : void {
+    console.log(this.boardName);
   }
 }
