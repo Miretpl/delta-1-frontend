08feb51d731d59b734cb35e841af30774d559e50
Przemysław Mirowski <miretpl@gmail.com>
2020-11-22 10:23:44 +0100

    working name change detect on enter and click

--- src/app/board/view-board/view-board.component.ts
+++ src/app/board/view-board/view-board.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,21 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewBoardComponent implements OnInit {
   boardId: number;
   boardName: string;
-  boardGroup: FormGroup;
-
+  
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.boardId = this.activatedRoute.snapshot.params.board_id;
-    
     this.boardName = "test board 1";
-
-    this.boardGroup = new FormGroup({
-      name: new FormControl(this.boardName, Validators.required)
-      });
   }
 
-  updateBoardName() : void {
-    console.log(this.boardName);
+  updateBoardName(value: string) {
+    if (this.boardName != value) {
+      // send request to backend
+      this.boardName = value;
+    } else {
+      console.log("board name without change")
+    }
   }
 }
