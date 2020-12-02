57dc02921322ab63b925245526b1c9bf37ebb94b
Patryk Janowski <52761148+raionsakana@users.noreply.github.com>
2020-12-02 22:20:58 +0100

    Add: logout on server side

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { UserService } from '../user/user.service';
+import { ApiService } from '../api/api.service';
+
 import { Router } from "@angular/router";
 
 @Component({
@@ -9,17 +11,28 @@ import { Router } from "@angular/router";
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  private ENDPOINT_NAME: string = "/logout";
+
   envName = environment.environmentName;
   isLogged: boolean;
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router, private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.userService.getLoggingStatus().subscribe((item: boolean) => this.isLogged = item);
     this.userService.emitLoggingStatus();
   }
 
-  logout() {
+  logout(): void {
+    this.apiService.logout(this.ENDPOINT_NAME).subscribe(
+      resp => this.handleLogout(),
+      error => console.error(error)
+    );
+
+    
+  }
+
+  private handleLogout(): void {
     this.userService.removeToken();
     this.userService.changeLoggingStatus();
     this.userService.emitLoggingStatus();
