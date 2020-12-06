2fc14a14b519db40a6bdb330a64bd5a556871da1
Przemysław Mirowski <miretpl@gmail.com>
2020-12-06 22:40:30 +0100

    implement view-card

--- src/app/board/view-board/view-board.component.ts
+++ src/app/board/view-board/view-board.component.ts
@@ -15,18 +15,23 @@ export class ViewBoardComponent implements OnInit {
   boardId: number;
   boardName: string;
   cardLists: any;
-  visibleAddList : boolean;
+  visibleAddList: boolean;
+  visibleCardView: boolean;
 
   constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.boardId = Number(this.activatedRoute.snapshot.params.board_id);
+    this.boardId = Number(this.activatedRoute.snapshot.params.boardId);
     this.getCardLists();
     this.interval = setInterval(() => this.getCardLists(), this.TWO_MINUTES);
   }
 
   ngOnDestroy(): void {
     clearInterval(this.interval);
+  }
+
+  visibleCardViewModal(): void {
+    this.visibleCardView = !this.visibleCardView;
   }
 
   visibleAddListForm(): void {
