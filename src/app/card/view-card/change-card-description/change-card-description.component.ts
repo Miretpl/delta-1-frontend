83f06f6fc893a541b010659646a9d2964df70c3d
Przemysław Mirowski <miretpl@gmail.com>
2020-12-18 21:10:45 +0100

    Fix: no description edit window open when desc is empty

--- src/app/card/view-card/change-card-description/change-card-description.component.ts
+++ src/app/card/view-card/change-card-description/change-card-description.component.ts
@@ -31,8 +31,6 @@ export class ChangeCardDescriptionComponent implements OnInit {
         },
         error => console.error(error)
       );
-
-      this.visibleChangeDescription.emit();
     }
   }
 
